2673bfe592f349bd6bff28dc0681b4ed6766a48a
lcarlossandberg <carlossandberg@googlemail.com>
2017-03-10 12:03:19 +0000

    doing args

--- type/t.m
+++ type/t.m
@@ -106,15 +106,15 @@ experimentrun::= Emptyrun|Exprun expression
 
 expression::= Emptyexpression
               |Ifelse expression expression expression
-              |Brackets expression
+              |Brackets expression ||-
               |List [argument]
-              |Operation expression op expression
+              |Operation expression op expression ||-
               |Funint [char] [argument]
               |Funext [char] [char] [argument]
-              |Varint [char]
-              |Varex [char]
+              |Varint [char] ||-
+              |Varex [char] ||-
               |Specialfunc specfunc expression
-              |Number num
+              |Number num ||-
               |Where expression [definition]
 
 op::= Plus
@@ -132,15 +132,16 @@ specfunc::= Listhead|Listtail|Listadd
 
 
 
-t2 = "t0_bid t = t0_order c_Bid 0 0 0"
-t3 = "1+3) 3"
+t2 = "t0_order c_Bid 0 0 0 t0_bid t = 4"
+t3 = "c_Bid 1 2 3 t0_bid t 4"
+t4 = "c_buy 1 2 3 c_bid 4 = 5"
 t1= "(1+2)+3 3 4"
 
 test = lex t1
 
 r = get_expression test Emptyexpression
-r2 = get_bracket (lex t3) []
-
+r2 = get_function (lex t2) []
+r3 = get_argsfun (lex t3) []
 
 
 
@@ -150,11 +151,17 @@ get_expression (Bra:xs) a = get_expression new_xs new_a
                             (in_bra, new_xs) = get_bracket xs []
                             (exp_inbra, rest) = get_expression in_bra Emptyexpression
                             new_a = (Brackets (exp_inbra))
-get_expression (x:xs) Emptyexpression = get_expression xs (match_exp x)
-get_expression x      a               = (a, x), if end_test x
-                                      = get_expression new_x new_a, otherwise
-                                        where
-                                        (new_x, new_a) = do_op x a
+get_expression ((Idfunc x y):xs) a = get_expression new_xs new_a
+                                     where
+                                     (arg_list, new_xs) = get_function xs []
+                                     ex_arglist = 
+                                     new_a = (Funext x y ex_arglist)
+
+get_expression (x:xs) Emptyexpression = get_expression xs (match_exp x) ||look at this again
+get_expression x      a = (a, x), if end_test x
+                        = get_expression new_x new_a, otherwise
+                          where
+                          (new_x, new_a) = do_op x a
 
 
 
@@ -163,17 +170,13 @@ get_expression x      a               = (a, x), if end_test x
 match_exp (Idnum x) = Number x
 match_exp (Idintvar x) = Varint x
 match_exp (Idvar x) = Varex x
-||match_exp (Bra:xs) = (br_exp, rest)
-||                     where
-||                     (br_exp, rest) = get_bracket xs []
+
 
 
 get_bracket (Bra:xs) a = get_bracket new_xs new_a
                          where
                          (new_a, new_xs) = get_innerbracket xs (a++[Bra])
 get_bracket (Ket:xs) a = (a, xs) ||((Brackets bra_internal), xs)
-                         ||where
-                         ||(bra_internal, rest) = get_expression a Emptyexpression
 get_bracket (x:xs)   a = get_bracket xs (a++[x])
 
 
@@ -200,6 +203,15 @@ match_op Opgreaterequ = Greaterequ
 
 end_test [] = True
 end_test (Opplus:xs) = False
+end_test (Opminus:xs) = False
+end_test (Opmult:xs) = False
+end_test (Opdivide:xs) = False
+end_test (Opless:xs) = False
+end_test (Opgreater:xs) = False
+end_test (Opequal:xs) = False
+end_test (Opnotequal:xs) = False
+end_test (Oplessequ:xs) = False
+end_test (Opgreaterequ:xs) = False
 end_test x = True
 
 
@@ -209,11 +221,20 @@ do_op (x:xs) a = (rest, (Operation a (match_op x) rest_a))
 
 
 
+get_function [] a = (a, [])
+get_function (Opequal:xs) a = (new_a, new_xs)
+                              where
+                              (mid_xs, new_a) = get_argsfun (reverse_list a []) []
+                              new_xs = mid_xs++[Opequal]++xs
+get_function (x:xs) a = get_function xs (a++[x])
 
 
+get_argsfun ((Idfunc x y):xs) a = ((Idfunc x y):a, (reverse_list xs []))
+get_argsfun ((Idcons x):xs)   a = ((Idcons x):a, (reverse_list xs []))
+get_argsfun (x:xs)            a = get_argsfun xs (a++[x])
 
-
-
+reverse_list []     a = a
+reverse_list (x:xs) a = reverse_list xs (x:a)
 
 
 
